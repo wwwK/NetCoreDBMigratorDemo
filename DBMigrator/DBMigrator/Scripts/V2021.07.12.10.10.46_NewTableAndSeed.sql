57c2ac577b193ae4b0a59bef331af9b285f8619c
Chris Cusack <chris.cusack@nbcc.ca>
2021-07-12 10:16:15 -0300

    Init

---
+++ DBMigrator/DBMigrator/Scripts/V2021.07.12.10.10.46_NewTableAndSeed.sql
@@ -0,0 +1,27 @@
+/* ****************************************************************
+ * Author: Chris
+ * Description: NewTableAndSeed
+ * ****************************************************************
+ */
+IF EXISTS (SELECT * from sysobjects where name='dbo.Things')
+	BEGIN
+		DROP TABLE [dbo].[Things]
+	END
+GO
+
+CREATE TABLE [dbo].[Things](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL
+CONSTRAINT [PK_Versions] PRIMARY KEY CLUSTERED 
+(
+[Id] ASC
+)) ON [PRIMARY]
+
+/*Seed things*/
+SET IDENTITY_INSERT Things ON
+
+INSERT INTO Things (Id, [Name]) VALUES(1,'Thing 1')
+INSERT INTO Things (Id, [Name]) VALUES(2,'Thing 2')
+INSERT INTO Things (Id, [Name]) VALUES(3,'Thing 3')
+
+SET IDENTITY_INSERT Things OFF
